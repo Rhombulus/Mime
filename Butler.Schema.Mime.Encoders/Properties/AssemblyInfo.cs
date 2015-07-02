b86ab5fe2ccd717db0a3cba4a7231d7d49e7022a
butlersoftware <thomas@butler.software>
2015-07-02 06:12:44 -0400

    cleanup code

--- Butler.Schema.Mime.Encoders/Properties/AssemblyInfo.cs
+++ Butler.Schema.Mime.Encoders/Properties/AssemblyInfo.cs
@@ -1,26 +1,28 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using System;
+using System.Linq;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Butler.Schema.Mime.Encoders")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Butler.Schema.Mime.Encoders")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+
+[assembly: System.Reflection.AssemblyTitle("Butler.Schema.Mime.Encoders")]
+[assembly: System.Reflection.AssemblyDescription("")]
+[assembly: System.Reflection.AssemblyConfiguration("")]
+[assembly: System.Reflection.AssemblyCompany("")]
+[assembly: System.Reflection.AssemblyProduct("Butler.Schema.Mime.Encoders")]
+[assembly: System.Reflection.AssemblyCopyright("Copyright ©  2015")]
+[assembly: System.Reflection.AssemblyTrademark("")]
+[assembly: System.Reflection.AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+
+[assembly: System.Runtime.InteropServices.ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2523d0a2-09ef-421c-a5fd-a88dfc7bc806")]
+
+[assembly: System.Runtime.InteropServices.Guid("2523d0a2-09ef-421c-a5fd-a88dfc7bc806")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,6 +34,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: InternalsVisibleTo("Butler.Schema.Mime")]
+
+[assembly: System.Reflection.AssemblyVersion("1.0.0.0")]
+[assembly: System.Reflection.AssemblyFileVersion("1.0.0.0")]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Butler.Schema.Mime")]
